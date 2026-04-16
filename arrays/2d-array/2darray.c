73b560b91b0c5049d5bef6e52026dc0a5e6c6cb6
M_omer <m.omerabid1@gmail.com>
2026-04-16 23:08:30 +0500

    2d-array
    practice of 2d array

---
+++ arrays/2d-array/2darray.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int arr[6][6];
+    
+    // Input 6x6 array
+    for(int i = 0; i < 6; i++) {
+        for(int j = 0; j < 6; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    // First hourglass sum as initial max
+    int maxSum =
+        arr[0][0] + arr[0][1] + arr[0][2]
+                  + arr[1][1]
+        + arr[2][0] + arr[2][1] + arr[2][2];
+
+    for(int i = 0; i <= 3; i++) {
+        for(int j = 0; j <= 3; j++) {
+
+            int sum =
+                arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                          + arr[i+1][j+1]
+                + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+
+            if(sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+    }
+
+    printf("%d\n", maxSum);
+
+    return 0;
+}
